2a8399a6be167b49f7a04e495a98c78a10adb0a1
DevWz <wellington.zaneze@outlook.com.br>
2019-05-27 15:40:20 -0300

    add navigation

---
+++ Src/NavigationView/NavigationView/Configuration.cs
@@ -0,0 +1,34 @@
+﻿using NavigationView.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace NavigationView
+{
+    public partial class MainPage : Page
+    {
+        public const string FEATURE_NAME = "Navigation View";
+
+        List<Scenario> pages = new List<Scenario>
+        {
+            new Scenario() { Title = "Home", Page = typeof(HomePage) },
+            new Scenario() { Title = "Page 1", Page = typeof(BlankPage_1) },
+            new Scenario() { Title = "Page 2", Page = typeof(BlankPage_2) },
+            new Scenario() { Title = "Page 3", Page = typeof(BlankPage_3) }
+        };
+    }
+
+    public class Scenario
+    {
+        public string Title { get; set; }
+        public Type Page { get; set; }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
--- Src/NavigationView/NavigationView/MainPage.xaml.cs
+++ Src/NavigationView/NavigationView/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -22,9 +23,79 @@ namespace NavigationView
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public static MainPage Current;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            Current = this;
         }
+
+        public List<Scenario> Pages
+        {
+            get { return this.pages; }
+        }
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigated += On_Navigated;
+
+            NavView.SelectedItem = NavView.MenuItems[0];
+
+            NavView_Navigate("Home", new EntranceNavigationTransitionInfo());
+        }
+
+        private void NavView_ItemInvoked(Windows.UI.Xaml.Controls.NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.InvokedItemContainer != null)
+            {
+                var navItem = args.InvokedItemContainer.Tag.ToString();
+                NavView_Navigate(navItem, args.RecommendedNavigationTransitionInfo);
+            }
+        }
+
+        private void NavView_SelectionChanged(Windows.UI.Xaml.Controls.NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.SelectedItemContainer != null)
+            {
+                var navItem = args.SelectedItemContainer.Tag.ToString();
+                NavView_Navigate(navItem, args.RecommendedNavigationTransitionInfo);
+            }
+        }
+
+        private void ContentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page" + e.SourcePageType.FullName);
+        }
+
+        private void On_Navigated(object sender, NavigationEventArgs e)
+        {
+            if (ContentFrame.SourcePageType != null)
+            {
+                Scenario item = Pages.FirstOrDefault(p => p.Page == e.SourcePageType);
+
+                NavView.SelectedItem = NavView.MenuItems
+                    .OfType<NavigationViewItem>()
+                    .First(n => n.Tag.Equals(item.Title));
+
+                NavView.Header = ((NavigationViewItem)NavView.SelectedItem)?.Content?.ToString();
+            }
+        }
+
+        void NavView_Navigate(string navItem, NavigationTransitionInfo navigationInfo)
+        {
+            Type page = null;
+            var item = Pages.FirstOrDefault(p => p.Title.Equals(navItem));
+            page = item.Page;
+
+            var preNavPageType = ContentFrame.CurrentSourcePageType;
+
+            if (!(page is null) && !Type.Equals(preNavPageType, page))
+            {
+                ContentFrame.Navigate(page, null, navigationInfo);
+            }
+        }
+
     }
 }
